327001783a93d897fb77d246aa002ad64e547804
Yurii Serhiichuk <savik.ne@gmail.com>
2019-11-07 18:35:31 +0200

    Make sure we use Bash as the default Shell for the GCE user

--- compute_engine/packer/_setup-user.sh
+++ compute_engine/packer/_setup-user.sh
@@ -3,7 +3,7 @@
 GCE_HOME_DIR="${GCE_HOME_DIR:-/usr/local/gce}"
 GCE_USER="${GCE_USER:-gce}"
 
-sudo useradd -m -d "${GCE_HOME_DIR}" "${GCE_USER}"
+sudo useradd -m -d "${GCE_HOME_DIR}" "${GCE_USER}" -s /bin/bash
 
 # This directory is usually used by GCloud to store the default credentials and overall GCloud configurations
 sudo mkdir -p "${GCE_HOME_DIR}/.config"
